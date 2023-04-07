1f0a2a0a75e7625b28f500526731272059c1fea0
Jack Young <66155970+YackMoon@users.noreply.github.com>
2023-04-07 14:31:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,16 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Code Happy!',
+  domain: 'codehappy.today',
+  author: 'Jack Young',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'YackMoon',
   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
